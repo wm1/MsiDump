7d3460d6a4c53bfd7939a4115927e4987f58a137
Wei Mao <weimao1@gmail.com>
2017-04-07 12:15:27 -0700

    Update version

--- version.h
+++ version.h
@@ -2,6 +2,6 @@
 // major.year.month.day
 //
 #define PROJ_VER_MAJOR  2
-#define PROJ_VER_MINOR  2017
-#define PROJ_VER_BUILD  4
-#define PROJ_VER_PATCH  7
+#define PROJ_VER_YEAR   2017
+#define PROJ_VER_MONTH  4
+#define PROJ_VER_DAY    7
